fbe99153c77e35f5724016e4b496eb8cd7b0bc61
themicp <themicp@gmail.com>
2025-03-24 13:38:04 +0000

    increase ticket creator sleep time to 1m

--- src/xrpl/ticket_creator.rs
+++ src/xrpl/ticket_creator.rs
@@ -37,7 +37,7 @@ impl XrplTicketCreator {
             }
         }
 
-        tokio::time::sleep(tokio::time::Duration::from_secs(30)).await
+        tokio::time::sleep(tokio::time::Duration::from_secs(60)).await
     }
 
     pub async fn run(&self) -> () {
