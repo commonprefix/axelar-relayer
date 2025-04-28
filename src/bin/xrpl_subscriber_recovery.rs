d414fd7459fcd0629f84fda7527cfa7424a0c5e6
themicp <themicp@gmail.com>
2025-04-28 10:58:05 +0000

    fix(xrpl-subscriber-recovery): gracefully close queue

--- src/bin/xrpl_subscriber_recovery.rs
+++ src/bin/xrpl_subscriber_recovery.rs
@@ -6,9 +6,10 @@ use axelar_relayer::{
     subscriber::Subscriber,
     utils::{setup_heartbeat, setup_logging},
 };
+use tokio::signal::unix::{signal, SignalKind};
 
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     dotenv().ok();
     let network = std::env::var("NETWORK").expect("NETWORK must be set");
     let config = Config::from_yaml(&format!("config.{}.yaml", network)).unwrap();
@@ -22,10 +23,21 @@ async fn main() {
 
     let mut subscriber = Subscriber::new_xrpl(&config.xrpl_rpc, redis_pool.clone()).await;
     let txs: Vec<&str> = vec![];
-    subscriber
-        .recover_txs(
+
+    let mut sigint = signal(SignalKind::interrupt())?;
+    let mut sigterm = signal(SignalKind::terminate())?;
+
+    tokio::select! {
+        _ = sigint.recv()  => {},
+        _ = sigterm.recv() => {},
+        _ = subscriber.recover_txs(
             txs.into_iter().map(|s| s.to_string()).collect(),
-            events_queue,
-        )
-        .await;
+            events_queue.clone(),
+        ) => {},
+
+    }
+
+    events_queue.close().await;
+
+    Ok(())
 }
