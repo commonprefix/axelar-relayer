3dd33898774abdd0bb95cf7db8e28bc5ccbb5a4c
Deiadara <nikos@commonprefix.com>
2025-06-10 15:44:31 +0300

    fix(xrpl_ticker_monitor): call setup_logging

--- xrpl/src/bin/xrpl_ticket_monitor.rs
+++ xrpl/src/bin/xrpl_ticket_monitor.rs
@@ -1,6 +1,6 @@
 use dotenv::dotenv;
 
-use relayer_base::config::Config;
+use relayer_base::{config::Config, utils::setup_logging};
 use tracing::{debug, error};
 use xrpl::client::XRPLClient;
 use xrpl_types::AccountId;
@@ -10,10 +10,11 @@ const THRESHOLD: i64 = 150;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    tracing_subscriber::fmt::init();
     dotenv().ok();
     let network = std::env::var("NETWORK").expect("NETWORK must be set");
     let config = Config::from_yaml(&format!("config.{}.yaml", network))?;
+
+    let _guard = setup_logging(&config);
 
     let client = XRPLClient::new(&config.xrpl_rpc, RETRIES)?;
     let account = AccountId::from_address(&config.xrpl_multisig).unwrap();
