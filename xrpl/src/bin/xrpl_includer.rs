3da718ab6413512e378db6387e1e7b851e2e00ca
Dejan Cabrilo <dcabrilo@gmail.com>
2025-06-27 18:36:50 +0200

    improvement: Separate configuration per-chain

--- xrpl/src/bin/xrpl_includer.rs
+++ xrpl/src/bin/xrpl_includer.rs
@@ -3,30 +3,30 @@ use std::sync::Arc;
 use tokio::signal::unix::{signal, SignalKind};
 
 use relayer_base::{
-    config::Config,
     database::PostgresDB,
     gmp_api,
     payload_cache::PayloadCache,
     queue::Queue,
     utils::{setup_heartbeat, setup_logging},
 };
-
+use relayer_base::config::{config_from_yaml};
+use xrpl::config::XRPLConfig;
 use xrpl::includer::XrplIncluder;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     dotenv().ok();
     let network = std::env::var("NETWORK").expect("NETWORK must be set");
-    let config = Config::from_yaml(&format!("config.{}.yaml", network)).unwrap();
+    let config: XRPLConfig = config_from_yaml(&format!("config.{}.yaml", network)).unwrap();
 
-    let _guard = setup_logging(&config);
+    let _guard = setup_logging(&config.common_config);
 
-    let tasks_queue = Queue::new(&config.queue_address, "includer_tasks").await;
-    let construct_proof_queue = Queue::new(&config.queue_address, "construct_proof").await;
-    let gmp_api = Arc::new(gmp_api::GmpApi::new(&config, true).unwrap());
-    let redis_client = redis::Client::open(config.redis_server.clone()).unwrap();
+    let tasks_queue = Queue::new(&config.common_config.queue_address, "includer_tasks").await;
+    let construct_proof_queue = Queue::new(&config.common_config.queue_address, "construct_proof").await;
+    let gmp_api = Arc::new(gmp_api::GmpApi::new(&config.common_config, true).unwrap());
+    let redis_client = redis::Client::open(config.common_config.redis_server.clone()).unwrap();
     let redis_pool = r2d2::Pool::builder().build(redis_client).unwrap();
-    let postgres_db = PostgresDB::new(&config.postgres_url).await.unwrap();
+    let postgres_db = PostgresDB::new(&config.common_config.postgres_url).await.unwrap();
     let payload_cache = PayloadCache::new(postgres_db);
     let xrpl_includer = XrplIncluder::new(
         config.clone(),
