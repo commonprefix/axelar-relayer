4894eac9c9a2159594418fd783192877a9c9f404
Dejan Cabrilo <dcabrilo@gmail.com>
2025-07-24 16:39:45 +0200

    Add heartbeat to ton

--- xrpl/src/bin/xrpl_heartbeat_monitor.rs
+++ xrpl/src/bin/xrpl_heartbeat_monitor.rs
@@ -1,9 +1,7 @@
 use dotenv::dotenv;
 
-use redis::Commands;
-use relayer_base::{utils::setup_logging};
-use tracing::{debug, error};
-use relayer_base::config::{config_from_yaml};
+use relayer_base::config::config_from_yaml;
+use relayer_base::heartbeat::heartbeats_loop;
 use xrpl::config::XRPLConfig;
 
 #[tokio::main]
@@ -11,46 +9,7 @@ async fn main() -> anyhow::Result<()> {
     dotenv().ok();
     let network = std::env::var("NETWORK").expect("NETWORK must be set");
     let config: XRPLConfig = config_from_yaml(&format!("config.{}.yaml", network))?;
+    let common_config = config.common_config.clone();
 
-    let redis_client = redis::Client::open(config.common_config.redis_server.clone())?;
-    let redis_pool = r2d2::Pool::builder().build(redis_client)?;
-
-    let client = reqwest::Client::new();
-
-    let _guard = setup_logging(&config.common_config);
-
-    loop {
-        debug!("Sending heartbeats to sentry monitoring endpoint");
-
-        for (key, url) in config.common_config.heartbeats.iter() {
-            let redis_key = format!("heartbeat:{}", key);
-            let mut redis_conn = redis_pool.get().unwrap();
-            if redis_conn.get(redis_key).unwrap_or(0) == 1 {
-                match client.get(url).send().await {
-                    Ok(response) => {
-                        if response.status().is_success() {
-                            debug!(
-                                "Successfully sent heartbeat to sentry monitoring endpoint for {}",
-                                key
-                            );
-                        } else {
-                            error!(
-                                "Failed to send heartbeat to sentry monitoring endpoint for {}: {:?}",
-                                key,
-                                response
-                            );
-                        }
-                    }
-                    Err(e) => {
-                        error!(
-                            "Failed to send heartbeat to sentry monitoring endpoint for {}: {}",
-                            key, e
-                        );
-                    }
-                }
-            }
-        }
-
-        tokio::time::sleep(std::time::Duration::from_secs(30)).await;
-    }
+    heartbeats_loop(&common_config).await
 }
