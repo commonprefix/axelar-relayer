dd7c162ae693d123e8d3a4fd50034b59f88d1cc3
Deiadara <nikos@commonprefix.com>
2025-09-10 15:41:57 +0300

    feat(subscriber): seperate subscriber into poller and listener

--- solana/src/lib.rs
+++ solana/src/lib.rs
@@ -5,9 +5,10 @@ pub mod gas_calculator;
 pub mod ingestor;
 pub mod models;
 pub use transaction_parser::parser;
-pub mod subscriber;
 pub mod transaction_parser;
 pub mod utils;
 pub use models::solana_transaction;
+pub mod subscriber_listener;
+pub mod subscriber_poller;
 
 pub mod test_utils;
