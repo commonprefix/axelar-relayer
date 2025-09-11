0b6a84698adbf07a567a3f7b32a021d438f9182b
Deiadara <nikos@commonprefix.com>
2025-09-11 17:53:05 +0300

    fix(migrations): add new fields to solana tx

--- migrations/0014_solana_transactions.sql
+++ migrations/0014_solana_transactions.sql
@@ -2,7 +2,9 @@ CREATE TABLE IF NOT EXISTS solana_transactions (
     signature               TEXT         PRIMARY KEY,
     slot                    BIGINT       NOT NULL,
     logs                    TEXT[],
+    ixs                     TEXT[],
     events                  TEXT[],
+    cost_units              BIGINT       DEFAULT 0,
     retries                 INTEGER      DEFAULT 10,
     created_at              TIMESTAMPTZ  NOT NULL DEFAULT NOW()
 );
